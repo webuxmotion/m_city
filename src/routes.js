fe6b33eb9fb2021a3cce40f224f26ef5ccbad787
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2019-04-13 12:55:46 +0300

    Start create Header and fix bug with babel runtime

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from './Hoc/Layout';
+
+const Routes = (props) => {
+  return(
+    <Layout>
+      Ssdf
+    </Layout>
+  );
+}
+
+export default Routes;
